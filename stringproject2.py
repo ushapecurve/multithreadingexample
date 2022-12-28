440bb1e5bd78497a9bb535dcb43bba275ec5b897
romanlukichev <roman.lukichev@yahoo.com>
2022-12-27 19:07:15 -0800

    added stringproject2.py. In that file I implemented reading string. But something was wrong. Because there was not two threads but only one thread. I meant to create two threads that read the same line.

---
+++ stringproject2.py
@@ -0,0 +1,21 @@
+import threading
+import time
+
+my_string = 'This is the string we shall be woking on today'
+
+def read_string(input_string, starting_position, in_step):
+    for letter in input_string[starting_position::in_step]:
+        print('%s thread is running: %s \n' % (threading.currentThread().getName(),  letter ))
+        time.sleep(0.5)
+        
+
+read_string(my_string, 0, 2)
+print("+++++++++++++++++++++++++++++++++++++++++++++++++++++")
+read_string(my_string, 1, 2)
+
+t1 = threading.Thread(target = read_string(my_string, 0, 2) , name = 'Thread-1-')
+t2 = threading.Thread(target = read_string(my_string, 1, 2), name = 'Thread-2-')
+        
+t1.start()
+t2.start() 
+    
